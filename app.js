057992e993f0f6e9e9727302249ecea926845d52
Павло <160887805+PaLyAnDrO@users.noreply.github.com>
2024-11-25 00:38:44 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+let mongoose = require('mongoose');
+let db = "mongodb+srv://Pavlo:9318@cluster0.uuko7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Node-blog";
+let methodOverride = require('method-override');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method"));
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// connect to database
+async function start () {
+  try {
+    await mongoose.connect(db);
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.log("\n Connection error! \n");
+  }
+}
+
+start();
+
+module.exports = app;
